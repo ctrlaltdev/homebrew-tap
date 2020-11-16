00f1f597d6d93eeee66aa2f0c9ccdb1d3fec439e
Yorick <yorick@ctrlalt.dev>
2020-11-16 13:40:38 -0800

    fix: install phase

--- Formula/memda.rb
+++ Formula/memda.rb
@@ -1,31 +1,31 @@
 class Memda < Formula
   VERSION = "v1.1.1".freeze
-  
-  SUMS = Hash[
-    'darwin-amd64' => '7e1ea3d8d499081e01fb98a9d5ef3862b95926910f386a9fcc4e0c753ae6a1c0',
-    'linux-amd64' => '172acc385347afb73c276625070cc2881d918c9ac8a3a352660cb783484d3c55',
-    'linux-386' => '723319ca7c77662a6a5154de3a70d0b9d73a6dacea08b17f9c19ec4c9dff81ab'
-  ]
-  RAW_ARCH = %x( uname -m )
-  RAW_OS = %x( uname -s )
+
+  SUMS = {
+    "darwin-amd64" => "7e1ea3d8d499081e01fb98a9d5ef3862b95926910f386a9fcc4e0c753ae6a1c0",
+    "linux-amd64" => "172acc385347afb73c276625070cc2881d918c9ac8a3a352660cb783484d3c55",
+    "linux-386" => "723319ca7c77662a6a5154de3a70d0b9d73a6dacea08b17f9c19ec4c9dff81ab"
+  }.freeze
+  RAW_ARCH = `uname -m`.freeze
+  RAW_OS = `uname -s`.freeze
 
   OS = RAW_OS == "Darwin" ? "darwin" : "linux"
   ARCH = RAW_ARCH == "x86_64" ? "amd64" : "386"
 
-  SHA256 = #{SUMS["#{OS}-${ARCH}"]}
+  SHA256 = # {SUMS["#{OS}-${ARCH}"]}
 
-  desc "Installs Memda from GitHub"
+  desc "AWS Lambda Memory Usage Checker"
   homepage "https://ctrlalt.dev/memda"
   url "https://github.com/ctrlaltdev/memda/releases/download/#{VERSION}/memda-#{OS}-#{ARCH}.tar.gz"
-  version #{VERSION}
-  sha256 #{SHA256}
+  version # {VERSION}
+  sha256 # {SHA256}
   license "MPL-2.0"
 
   def install
-    bin.install "memda-#{OS}-#{ARCH}" => 'memda'
+    bin.install "memda-#{OS}-#{ARCH}" => "memda"
   end
 
   test do
-   system "#{bin}/memda"
+    system "#{bin}/memda"
   end
 end
