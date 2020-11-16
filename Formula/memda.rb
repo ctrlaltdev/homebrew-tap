08f88c98d55b3a017c936e11795586662c689870
Yorick <yorick@ctrlalt.dev>
2020-11-16 12:27:47 -0800

    fix: install phase

--- Formula/memda.rb
+++ Formula/memda.rb
@@ -6,9 +6,10 @@ class Memda < Formula
   url "https://github.com/ctrlaltdev/memda/releases/download/#{VERSION}/memda-darwin-amd64.tar.gz"
   version #{VERSION}
   sha256 '7e1ea3d8d499081e01fb98a9d5ef3862b95926910f386a9fcc4e0c753ae6a1c0'
+  license "MPL-2.0"
 
   def install
-    bin.install 'memda'
+    bin.install 'bin/memda-darwin-amd64'
   end
 
   test do
