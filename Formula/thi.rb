ef515d1a34243f3bf19c0c64a86e33ff69a032aa
Yorick <yorick@ctrlalt.dev>
2021-04-12 20:01:31 -0700

    feat: bump thi to v1.1.0

--- Formula/thi.rb
+++ Formula/thi.rb
@@ -1,12 +1,12 @@
 class Thi < Formula
-  VERSION = "v1.0.0".freeze
+  VERSION = "v1.1.0".freeze
 
   # rubocop:disable Layout/HashAlignment
   SUMS = Hash[
-    "linux-amd64"  => "df6a7c9c27510e1730303a431edbb90ce517f9462afeec4b43a41529bc3b5654",
-    "linux-arm64"  => "6d38ca5a09c31658844b1483267d2c2682e2edcc4aaf2381b8f6ec2051cbf5c7",
-    "darwin-amd64" => "981d69d0157d070bcfb7e41e3ab57a12dbb08e020f214008eb659828b1f1e5a7",
-    "darwin-arm64" => "e6394dade1a0528dedec487ddcdd47e5fb85526a71aca01ec2e8e9a1f8507e80",
+    "darwin-amd64" => "1f08a5aced923bd9715dffcc89f247bcd92a1024e55eb5e5581e95a21e46c658",
+    "darwin-arm64" => "0be6ed3a4aabc543831be524e562eef2b7a4fbf66b42be28f7dbfd33b95836cc",
+    "linux-amd64"  => "dd6c4b602c4748149fad3784a295bc882944fddcf7f315404e93842920be120d",
+    "linux-arm64"  => "c659fb60590548dfbb25f025b299880d6b6968156433b9343cd8f6b370657117",
   ].freeze
   # rubocop:enable Layout/HashAlignment
   RAW_OS = `uname -s`.freeze
